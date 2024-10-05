1e8231f6d067af5b8827c2343c6ec901039599c8
Carlos A Santos <carlos.itdeveloper@gmail.com>
2024-10-05 17:16:15 -0300

    first commit

---
+++ src/app/glspages/pedidos/pedido-add/pedido-add.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedido-add',
+  standalone: true,
+  imports: [],
+  templateUrl: './pedido-add.component.html',
+  styleUrl: './pedido-add.component.scss'
+})
+export class PedidoAddComponent {
+
+}
---
+++ src/app/glspages/pedidos/pedido-upd/pedido-upd.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedido-upd',
+  standalone: true,
+  imports: [],
+  templateUrl: './pedido-upd.component.html',
+  styleUrl: './pedido-upd.component.scss'
+})
+export class PedidoUpdComponent {
+
+}
